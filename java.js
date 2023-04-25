585ecce86789612643dcff5bb234574caa152c8d
que veus tonto, ves cap allà <122235845+00TONI00@users.noreply.github.com>
2023-04-25 21:54:00 +0200

    d

--- java.js
+++ java.js
@@ -20,25 +20,3 @@ photo.style.transform = "scale(1)";
 }
 /*------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------*/
-function mezclarColor(color1, color2) {
-    var mezcla = "#";
-    for (var i = 1; i < 7; i += 2) {
-      var parte1 = parseInt(color1.substr(i, 2), 16);
-      var parte2 = parseInt(color2.substr(i, 2), 16);
-      var parteMezclada = Math.floor((parte1 + parte2) / 2).toString(16);
-      mezcla += ("00" + parteMezclada).substr(-2);
-    }
-    return mezcla;
-  }
-  
-  function cambiarColor() {
-    var colorActual = document.body.style.backgroundColor;
-    var colorNuevo = Math.floor(Math.random() * 2) == 0 ? "#fff" : "#000";
-    var colorMezclado = mezclarColor(colorActual, colorNuevo);
-    document.body.style.backgroundColor = colorMezclado;
-    setTimeout(cambiarColor, 1000);
-  }
-  
-  cambiarColor();
-/*------------------------------------------------------------------------------------------------*/
-/*------------------------------------------------------------------------------------------------*/
